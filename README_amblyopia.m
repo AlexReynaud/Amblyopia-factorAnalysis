109559c2e79b629b16355e75b82cb635c306db6d
AlexReynaud <43037174+AlexReynaud@users.noreply.github.com>
2023-04-21 12:02:14 -0400

    Add files via upload

---
+++ README_amblyopia.m
@@ -0,0 +1,51 @@
+%%
+
+
+%% data structure
+
+load('dataset_amblyopia.mat')
+data
+
+% data = 
+% 
+%   struct with fields:
+% 
+%     sensitivityAch: [2×12×51 double]
+%      sensitivityRG: [2×8×51 double]
+%      sensitivityBY: [2×8×51 double]
+%     frequenciesAch: [0.2500 0.3482 0.4850 0.6755 0.9408 1.3104 1.8251 2.5420 3.5405 4.9313 6.8683 9.5662]
+%      frequenciesRG: [0.2500 0.3482 0.4850 0.6755 0.9408 1.3104 1.8251 2.5420]
+%      frequenciesBY: [0.2500 0.3482 0.4850 0.6755 0.9408 1.3104 1.8251 2.5420]
+% 
+% the 3 sensitivty fields are the cone contrast sensitivities for Achromatic, 
+% Red-Green and Blue-Yellow stimuli
+% the 3 dimensions are:
+% - 2 repetitions
+% - 12 or 8 frequencies tested
+% - 51 subjects
+% 
+% the 3 frequencies fields are the frequencies tested for each condition
+
+
+%% example plot of the cone-contrast sensitivity over the population
+
+figure('Name','Cone-contrast sensitivty')
+hold on
+% plot(data.frequencies.Reynaud,mean(mean(data.sensitivities.noise_patches.amblyopes.Reynaud.tempFE,3),1),'k')
+
+plot(data.frequencies.Zhou_Beylerian,mean(mean(data.sensitivities.noise_patches.amblyopes.Zhou_Beylerian.tempFE,3),1),'r')
+plot(data.frequencies.Zhou_Beylerian,mean(mean(data.sensitivities.noise_patches.amblyopes.Zhou_Beylerian.tempAE,3),1),'r')
+
+plot(data.frequencies.Gao,mean(mean(data.sensitivities.noise_patches.amblyopes.Gao.tempFE,3),1),'r')
+plot(data.frequencies.Gao,mean(mean(data.sensitivities.noise_patches.amblyopes.Gao.tempAE,3),1),'r')
+% plot(data.frequenciesRG,mean(mean(data.sensitivityRG,3),1),'r')
+% plot(data.frequenciesBY,mean(mean(data.sensitivityBY,3),1),'b')
+set(gca,'XScale','log','YScale','log')
+axis square 
+grid on
+xlabel('Spatial frequency (c/d)')
+ylabel('Sensitivity')
+
+legend({'Ach.','RG','BY'})
+
+
